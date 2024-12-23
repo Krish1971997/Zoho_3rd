9778ab96b48c421f3a249710bc5d4574e6fd3a01
Krish1971997 <krishnakumar1971997@gmail.com>
2024-12-23 22:01:46 +0530

    first commit

--- src/carRentalSystem/Notification.java
+++ src/carRentalSystem/Notification.java
@@ -1,5 +1,5 @@
 package carRentalSystem;
 
 public class Notification {
-
+	
 }
--- src/carRentalSystem/VehicleLog.java
+++ src/carRentalSystem/VehicleLog.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class VehicleLog {
+	
 	Map<String, List<Log>> log = new HashMap<>();
 	private static VehicleLog instance = null;
 
