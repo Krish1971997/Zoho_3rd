8d453bfc608229e713bf98fac1b6cdccf84a3a62
Krish1971997 <krishnakumar1971997@gmail.com>
2025-02-24 00:20:24 +0530

    Commit on 24/02/2025  0:20:24.01

--- src/lunchHallBooking/LunchHall.java
+++ src/lunchHallBooking/LunchHall.java
@@ -3,37 +3,37 @@ package lunchHallBooking;
 import java.util.List;
 
 public class LunchHall {
-    private int hallId;
-    private String name;
-    private String location;
-    private int capacity;
-    private List<String> amenities;
+	private int hallId;
+	private String name;
+	private String location;
+	private int capacity;
+	private List<String> amenities;
 
-    public LunchHall(int hallId, String name, String location, int capacity, List<String> amenities) {
-        this.hallId = hallId;
-        this.name = name;
-        this.location = location;
-        this.capacity = capacity;
-        this.amenities = amenities;
-    }
+	public LunchHall(int hallId, String name, String location, int capacity, List<String> amenities) {
+		this.hallId = hallId;
+		this.name = name;
+		this.location = location;
+		this.capacity = capacity;
+		this.amenities = amenities;
+	}
 
-    public int getHallId() {
-        return hallId;
-    }
+	public int getHallId() {
+		return hallId;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getLocation() {
-        return location;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public int getCapacity() {
-        return capacity;
-    }
+	public int getCapacity() {
+		return capacity;
+	}
 
-    public List<String> getAmenities() {
-        return amenities;
-    }
+	public List<String> getAmenities() {
+		return amenities;
+	}
 }
